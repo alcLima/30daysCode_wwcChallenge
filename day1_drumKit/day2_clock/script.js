dbbc123dc4a6a0fbf199c7b32f1e93309b2776d3
Ana Luiza <alclim02@gmail.com>
2024-01-14 16:09:12 -0500

    drumKit back

---
+++ day1_drumKit/day2_clock/script.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const currentSeconds = now.getSeconds();
+  const secondsDegrees = (currentSeconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(currentSeconds, secondsDegrees);
+
+  if(currentSeconds == 0){
+    secondHand.addcla
+  }
+
+  const currentMinutes = now.getMinutes();
+  const minutesDegrees = (currentMinutes / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const currentHours = now.getHours();
+  const hoursDegrees = (currentHours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
